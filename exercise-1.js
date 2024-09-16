63c89b6b6480ff19e4b2b909eeaa3350775a4622
Stephane Masyn <stefimaz@gmail.com>
2024-09-16 12:46:13 -0400

    Update exercise-1.js

--- exercise-1.js
+++ exercise-1.js
@@ -4,3 +4,26 @@ Start with exercise-1.md if you haven't read it!
 */
 
 // YOUR WORK GOES HERE //
+const bond45 = {
+   name: "Bond 45",
+   cuisine: ["Italian", "steaks"],
+   numberOfStars: 4,
+   favorited: true
+};
+
+bond45.address = "123 main st";
+bond45.zipcode = 10012;
+bond45.acceptsReservations = true;
+
+bond45.numberOfStars += 1;
+bond45.favorited = !bond45.favorited;
+bond45.cuisine.push("pastas");
+
+function retrieveProperty(key, obj) {
+   if (obj[key]) {
+      return object[key];
+   } else {
+      return "The information you requested does not exist."
+   }
+};
+
